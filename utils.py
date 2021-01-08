f79cb81de13482b32e1d272d3b3e2a912dc3ccf3
SlyFoxer <57837488+SlyFoxer@users.noreply.github.com>
2021-01-08 18:54:29 -0500

    Rewrite all functions as while loops

--- utils.py
+++ utils.py
@@ -4,47 +4,44 @@ def print_message():
 
 # Drink type function
 def get_drink_type():
-  res = input('What type of drink would you like? \n[a] Brewed Coffee \n[b] Mocha \n[c] Latte \n[d] Tea \n> ')
+  while True:
+    res = input('What type of drink would you like? \n[a] Brewed Coffee \n[b] Mocha \n[c] Latte \n[d] Tea \n> ')
 
-  if res == 'a':
-    return order_brewed_coffee()
-  elif res == 'b':
-    return order_mocha()
-  elif res == 'c':
-    return order_latte()
-  elif res == 'd':
-    return order_tea()
-  else:
+    if res == 'a':
+      return order_brewed_coffee()
+    elif res == 'b':
+      return order_mocha()
+    elif res == 'c':
+      return order_latte()
+    elif res == 'd':
+      return order_tea()
     print_message()
-    return get_drink_type()
 
 # Drink size function  
 def get_size():
-  res = input('What size drink can I get for you? \n[a] Small \n[b] Medium \n[c] Large \n> ')
+  while True:
+    res = input('What size drink can I get for you? \n[a] Small \n[b] Medium \n[c] Large \n> ')
   
-  if res == 'a':
-    return 'small'
-  elif res == 'b':
-    return 'medium'
-  elif res == 'c':
-    return 'large'
-  else:
+    if res == 'a':
+      return 'small'
+    elif res == 'b':
+      return 'medium'
+    elif res == 'c':
+      return 'large'
     print_message()
-    return get_size()
 
 # Latte milk option function
 def order_latte():
-  res = input('And what kind of milk for your latte? \n[a] 2% milk \n[b] Non-fat milk \n[c] Soy milk \n> ')
+  while True:
+    res = input('And what kind of milk for your latte? \n[a] 2% milk \n[b] Non-fat milk \n[c] Soy milk \n> ')
 
-  if res == 'a':
-    return 'latte'
-  elif res == 'b':
-    return 'non-fat latte'
-  elif res == 'c':
-    return 'soy latte'
-  else:
+    if res == 'a':
+      return 'latte'
+    elif res == 'b':
+      return 'non-fat latte'
+    elif res == 'c':
+      return 'soy latte'
     print_message()
-    return order_latte()
 
 # Mocha options function
 def order_mocha():
